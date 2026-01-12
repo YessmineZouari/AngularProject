6b2dbb503116f3bcaf5ea8e0e80be22fe8383eb6
dahrook <marrakchi.abderrahmen@gmail.com>
2026-01-12 14:32:50 +0100

    all things are merged

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,39 +5,38 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MemberComponent } from './member/member.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatTableModule} from '@angular/material/table';
+import { MatTableModule } from '@angular/material/table';
 import { HttpClientModule } from '@angular/common/http';
 import { MemberFormComponent } from './member-form/member-form.component';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 import { ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.component';
-import {MatDialogModule} from '@angular/material/dialog';
+import { MatDialogModule } from '@angular/material/dialog';
 import { TemplateComponent } from './template/template.component';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatListModule} from '@angular/material/list';
-import {MatMenuModule} from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ToolComponent } from './tool/tool.component';
+import { ToolCreateComponent } from './tool-create/tool-create.component'; // <--- NEW
 import { ArticleComponent } from './article/article.component';
 import { EventComponent } from './event/event.component';
-import { MatPaginatorModule} from '@angular/material/paginator';
-import { MatSortModule} from '@angular/material/sort';
 import { EventCreateComponent } from './event-create/event-create.component';
-import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { firebaseConfig } from './environment';
-import {AngularFireModule} from "@angular/fire/compat" ;
-import {AngularFireAuthModule} from "@angular/fire/compat/Auth";
-import { LoginComponent } from './login/login.component'
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/Auth';
+import { LoginComponent } from './login/login.component';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-
 import { MatSelectModule } from '@angular/material/select';
 import { ArticleCreateComponent } from './article-create/article-create.component';
 import { NgChartsModule } from 'ng2-charts';
-
 import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
@@ -49,12 +48,12 @@ import { MatCardModule } from '@angular/material/card';
     TemplateComponent,
     DashboardComponent,
     ToolComponent,
+    ToolCreateComponent, // <--- ADDED
     ArticleComponent,
     EventComponent,
     EventCreateComponent,
     LoginComponent,
     ArticleCreateComponent
-    
   ],
   imports: [
     BrowserModule,
@@ -62,24 +61,26 @@ import { MatCardModule } from '@angular/material/card';
     BrowserAnimationsModule,
     MatTableModule,
     HttpClientModule,
-    MatFormFieldModule, //api men angular material
+    MatFormFieldModule,
     MatInputModule,
-    ReactiveFormsModule, // pour formGroup
+    ReactiveFormsModule,
     FormsModule,
-    MatIconModule, 
+    MatIconModule,
     MatDialogModule,
-    MatSidenavModule,//template
-    MatToolbarModule,//template
-    MatListModule,//template
-    MatMenuModule, //template
-   MatPaginatorModule,
-   MatSortModule,MatDatepickerModule,MatNativeDateModule,
-   AngularFireModule.initializeApp(firebaseConfig),AngularFireAuthModule,
-   MatProgressSpinnerModule,
-   MatSelectModule,
-   NgChartsModule, 
-   MatCardModule
-
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatMenuModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    MatProgressSpinnerModule,
+    MatSelectModule,
+    NgChartsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
